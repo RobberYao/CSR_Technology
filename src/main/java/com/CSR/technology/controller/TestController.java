5576954a4651b9d14b62cd01128c3cc2b192d7f0
RobberYao <ychh1015@sina.cn>
2017-11-15 11:16:07 +0800

    update

--- src/main/java/com/CSR/technology/controller/TestController.java
+++ src/main/java/com/CSR/technology/controller/TestController.java
@@ -2,14 +2,13 @@ package com.CSR.technology.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.util.Assert;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.CSR.technology.entity.User;
 import com.CSR.technology.service.UserService;
 
-@RestController
+@Controller
 public class TestController {
 
 	@Autowired
